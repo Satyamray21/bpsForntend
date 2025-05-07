3405c2836f72a49453e3bc46ec0c3d714f217310
Satyamray21 <satyamray43@gmail.com>
2025-05-07 14:47:19 +0530

    added store for customer slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import locationReducer from '../features/Location/locationSlice';
 import stationReducer from '../features/stations/stationSlice';
-
+import customerReducer from '../features/customers/customerSlice'
 export const store = configureStore({
   reducer: {
     stations: stationReducer,
     location: locationReducer,
+    customers: customerReducer,
   },
 });
