6bf91eb29b7d358f75decf277581eac4fddda597
Satyamray21 <satyamray43@gmail.com>
2025-05-08 14:45:22 +0530

    added driverReducers.

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import locationReducer from '../features/Location/locationSlice';
 import stationReducer from '../features/stations/stationSlice';
 import customerReducer from '../features/customers/customerSlice'
+import driverReducer from '../features/Driver/driverSlice'
 export const store = configureStore({
   reducer: {
     stations: stationReducer,
     location: locationReducer,
     customers: customerReducer,
+    drivers: driverReducer,
+
   },
 });
