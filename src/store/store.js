1bfd35e31f6f6938994d5a8a7f7c8224279c24ec
Satyamray21 <satyamray43@gmail.com>
2025-05-10 15:30:54 +0530

    added reducers.

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,14 @@ import locationReducer from '../features/Location/locationSlice';
 import stationReducer from '../features/stations/stationSlice';
 import customerReducer from '../features/customers/customerSlice'
 import driverReducer from '../features/Driver/driverSlice'
+import bookingReducer from '../features/booking/bookingSlice'
 export const store = configureStore({
   reducer: {
     stations: stationReducer,
     location: locationReducer,
     customers: customerReducer,
     drivers: driverReducer,
+    bookings:bookingReducer,
 
   },
 });
